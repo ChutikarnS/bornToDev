dfa4d20a0f437604988a5389385cdbfa1a9a021c
ChutikarnS <cchutikarnn@gmail.com>
2021-07-03 19:27:22 +0700

    homework
    homework

---
+++ ArrayListApp/ArrayListApp/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArrayListApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        ArrayList arrayList = new ArrayList();
+        public MainWindow()
+        {
+            InitializeComponent();
+            arrayList = new ArrayList();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("ADD>>" + txtBox.Text);
+            if (txtBox.Text.Equals(""))
+            {
+                MessageBox.Show("Please enter your data");
+            }
+            else
+            {
+                arrayList.Add(txtBox.Text);
+                MessageBox.Show("Add Success");
+            }
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Remove>>" + txtBox.Text);
+            if (txtBox.Text.Equals(""))
+            {
+                MessageBox.Show("Please enter your data");
+            }
+            else
+            {
+                arrayList.Remove(txtBox.Text);
+                MessageBox.Show("Remove Success");
+            }
+        }
+
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            if (arrayList.Count <= 0)
+            {
+                MessageBox.Show("No data");
+            }
+            else
+            {
+                foreach (string data in arrayList)
+                {
+                    MessageBox.Show(data);
+                }
+            }
+        }
+
+        /*private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Add Data
+            arrayList.Add(5);
+            arrayList.Add(3);
+            arrayList.Add(1);
+            arrayList.Add(8);
+            arrayList.Add(2);
+            arrayList.Add(9);
+
+            //Show Data
+            foreach (int data in arrayList)
+            {
+                MessageBox.Show(data.ToString());
+            }
+
+            //Count
+            MessageBox.Show("Count " + arrayList.Count.ToString());
+
+            //Remove
+            //arrayList.Remove(10); //Object
+            //arrayList.RemoveAt(10); //index
+
+            //Sort
+            arrayList.Sort();
+
+            //Clear
+            arrayList.Clear();
+
+        }*/
+
+    }
+}
---
+++ HashTable/HashTable/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HashTable
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hashtable hashtable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //Add
+            if(txtKey.Text.Equals("")){
+                MessageBox.Show("Please enter your key");
+            }else if (txtValue.Text.Equals(""))
+            {
+                MessageBox.Show("Please enter your value");
+            }
+            else
+            {
+                bool checkKey = hashtable.ContainsKey(txtKey.Text);
+                Console.WriteLine("checkKey>>" + checkKey);
+                if (checkKey)
+                {
+                    MessageBox.Show("have data");
+                }
+                else
+                {
+                    hashtable.Add(txtKey.Text, txtValue.Text);
+                    MessageBox.Show("Add Success");
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            //Remove
+            if (txtKey.Text.Equals(""))
+            {
+                MessageBox.Show("Please enter your key");
+            }
+            else
+            {
+                if (hashtable.Count <= 0)
+                {
+                    MessageBox.Show("No Data");
+                }
+                else
+                {
+                    hashtable.Remove(txtKey.Text);
+                    MessageBox.Show("Remove Success");
+                }
+            }
+        }
+
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            ICollection icollection = hashtable.Keys;
+
+            //Show All
+            if (hashtable.Count <= 0)
+            {
+                MessageBox.Show("No Data");
+            }
+            else
+            {
+                foreach (string key in icollection)
+                {
+                    MessageBox.Show(hashtable[key].ToString());
+                }
+            }
+        }
+    }
+}
